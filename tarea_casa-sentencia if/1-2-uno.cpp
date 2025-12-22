2609fec332dcc7e2c5c0fa66f1972f62d8ed0ad2
Eduardo Ayachi <paredes.eddma@gmail.com>
2025-12-22 03:24:00 -0500

    practicando con dificultad

---
+++ tarea_casa-sentencia if/1-2-uno.cpp
@@ -0,0 +1,49 @@
+// Los trabajadores de una fábrica tienen tres turnos: mañana, tarde y noche. La tarifa de los turnos de la mañana 
+// y tarde son iguales, mientras que de noche son 30% mayores. Construye un programa que lea el número de horas laboradas
+// por un trabajador y su tarifa. Calcula el salario semanal. Considera que durante la semana el trabajador se
+// encuentra siempre en el mismo turno.
+
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "es_ES.UTF-8");
+
+    char turno;
+    double horas, salario = 0,tarifa = 0;
+
+    cout << "\t\033[33;4mCalcule tu salario laboral\033[0m\n";
+    cout << "\nElige tu turno laboral de la semana\n";
+    cout << "Mañana (m), Tarde (t), Noche (n): ";
+    cin >> turno;
+
+    if (turno != 'm' && turno != 'M' && turno != 't' && turno != 'T' && turno != 'n' && turno != 'N'){
+        cout << "El turno escogido es inválido." << endl;
+        return 0;
+    }
+    
+    cout << "Ingrese la tarifa de tu hora: ";
+    cin >> tarifa;
+    cout << "Ingresa tus horas laboradas de la semana: ";
+    cin >> horas;
+    
+
+    if (horas <= 0 || tarifa <= 0){
+        cout << "Ingrese una hora y/o tarifa correcta." << endl;
+        return 0;
+    }
+
+   
+    if (turno == 'm' || turno == 'M' || turno == 't' || turno == 'T'){
+        salario = tarifa * horas;
+        // cout << "El salario semanal de tu turno correspondiente es: " << salario << endl;
+    } else {
+        salario = (tarifa + tarifa * 0.3) * horas;
+        // cout << "El salario semanal de tu turno correspondiente es: " << salario << endl;
+    }
+
+    cout << "El salario semanal de tu turno correspondiente es: " << salario << endl;
+
+    return 0;
+}
---
+++ tarea_casa-sentencia if/2-dos.cpp
@@ -0,0 +1,9 @@
+// A un trabajador se le paga una tarifa fija las horas normales de
+// labores. Se consideran horas normales a todas las horas trabajadas hasta
+// un total de 140 al mes. Las horas adicionales a 140 se consideran
+// extras. Por una hora extra se paga el equivalente a 1,5 horas normales.
+// Los impuestos varían según el monto obtenido por el trabajador. Si el
+// sueldo es menor o igual a S/. 2000 y no paga impuestos, por los
+// siguientes S/. 2000 para el 10%, mientras que para montos mayores a S/.
+// 4000 para el 15% sobre el adicional, ¿Cuál es el sueldo neto del
+// trabajador y cuál es el monto en impuestos que debe pagar?
